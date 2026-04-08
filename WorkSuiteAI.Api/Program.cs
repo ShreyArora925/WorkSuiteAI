d5a2966e70f9d9b05e284a143a7b4214c9fc0c0d
Shreyarora964 <sarora179@myseneca.ca>
2026-04-08 16:30:17 -0400

    feat: add FluentValidation for CreateEmployee Service

--- WorkSuiteAI.Api/Program.cs
+++ WorkSuiteAI.Api/Program.cs
@@ -1,13 +1,15 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
-
 using System.Text;
 using WorkSuiteAI.Application.Handlers;
 using WorkSuiteAI.Application.Interfaces;
 using WorkSuiteAI.Application.Queries;
 using WorkSuiteAI.Application.Services;
+using WorkSuiteAI.Application.Validators;
 using WorkSuiteAI.Domain.Interfaces;
 using WorkSuiteAI.Infrastructure.Data;
 using WorkSuiteAI.Infrastructure.Repositories;
@@ -19,15 +21,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+//builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetAllEmployeesHandler).Assembly));
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateEmployeeValidator>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITimeEntryService, TimeEntryService>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
@@ -57,9 +60,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
-    app.MapOpenApi();   
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //app.MapOpenApi();   
 }
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 if (!app.Environment.IsDevelopment())
---
+++ WorkSuiteAI.Application/Validators/CreateEmployeeValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using WorkSuiteAI.Application.DTO;
+
+namespace WorkSuiteAI.Application.Validators
+{
+    public class CreateEmployeeValidator :AbstractValidator<CreateEmployeeRequest>
+    {
+        public CreateEmployeeValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First name is required")
+                .MaximumLength(50).WithMessage("First name cannot exceed 50 characters");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required")
+                .MaximumLength(50).WithMessage("Last name cannot exceed 50 characters");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format")
+                .MaximumLength(100).WithMessage("Email cannot exceed 100 characters");
+
+            RuleFor(x => x.Department)
+                .NotEmpty().WithMessage("Department is required")
+                .MaximumLength(50).WithMessage("Department cannot exceed 50 characters");
+
+            RuleFor(x => x.HourlyRate)
+                .GreaterThanOrEqualTo(16.55m).WithMessage("Hourly rate must be at least minimum wage ($16.55)")
+                .LessThanOrEqualTo(100).WithMessage("Hourly rate cannot exceed $100");
+        }
+    }
+}
